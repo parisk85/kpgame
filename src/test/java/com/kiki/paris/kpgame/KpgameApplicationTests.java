4bc51f5e28b8bd42f2fa784d5d58c53ac2cfb5da
parisk85 <PKRDAM&pkgsdo7>
2017-09-10 17:53:58 +0300

    Initial commit

---
+++ src/test/java/com/kiki/paris/kpgame/KpgameApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kiki.paris.kpgame;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KpgameApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
